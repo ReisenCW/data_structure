50464f27539e76640f5e280ab9d3f9e59e75c5f7
ReisenCW <814568436@qq.com>
2024-11-21 13:26:27 +0800

    refine2

--- 2_2350284/2_2350284.cpp
+++ 2_2350284/2_2350284.cpp
@@ -5,7 +5,6 @@ using namespace std;
 class List;
 List GetIntersection(const List& S1, const List& S2);
 
-
 class Node {
 	friend class List;
 	friend List GetIntersection(const List& S1, const List& S2);
@@ -24,56 +23,59 @@ private:
 	int size;
 public:
 	List(): head(nullptr), tail(nullptr),size(0) {}
-
-	void push_back(int num) {
-		if(head == nullptr)//链表为空
-		{
-			head = new Node(num);
-			tail = head;
-		}
-		else//链表不为空
-		{
-			Node* p = new Node(num);
-			tail->next = p;
-			p->prev = tail;
-			tail = p;
-		}
-		size++;
-	}
-
-	void printList()const {
-		if (size == 0) {
-			cout << "NULL" << endl;
-			return;
-		}
-		//输出链表中的元素,以空格分隔,最后一个元素后面没有空格
-		Node* p = head;
-		cout << p->data;
-		p = p->next;
-		while (p) {
-			cout << " " << p->data;
-			p = p->next;
-		}
-	}
-
-	List(const List& L) : head(nullptr), tail(nullptr), size(0)
-	{
-		Node* p = L.head;
-		while (p) {
-			push_back(p->data);
-			p = p->next;
-		}
-	}
-
-	~List() {
-		Node* p = head;
-		while (p) {
-			Node* q = p;
-			p = p->next;
-			delete q;
-		}
-	}
+	void push_back(int num);
+	void PrintList() const;
+	List(const List& L);
+	~List();
 };
+
+List::List(const List& L) : head(nullptr), tail(nullptr), size(0)
+{
+	Node* p = L.head;
+	while (p) {
+		push_back(p->data);
+		p = p->next;
+	}
+}
+
+List::~List() {
+	Node* p = head;
+	while (p) {
+		Node* q = p;
+		p = p->next;
+		delete q;
+	}
+}
+void List::push_back(int num) {
+	if (head == nullptr)//链表为空
+	{
+		head = new Node(num);
+		tail = head;
+	}
+	else//链表不为空
+	{
+		Node* p = new Node(num);
+		tail->next = p;
+		p->prev = tail;
+		tail = p;
+	}
+	size++;
+}
+
+void List::PrintList()const {
+	if (size == 0) {
+		cout << "NULL" << endl;
+		return;
+	}
+	//输出链表中的元素,以空格分隔,最后一个元素后面没有空格
+	Node* p = head;
+	cout << p->data;
+	p = p->next;
+	while (p) {
+		cout << " " << p->data;
+		p = p->next;
+	}
+}
 
 List GetIntersection(const List& S1, const List & S2) {
 	List S3;
@@ -117,7 +119,7 @@ int main() {
 	//求两个链表的交集
 	List S3 = GetIntersection(S1, S2);
 	//输出交集
-	S3.printList();
+	S3.PrintList();
 	system("pause");
 	return 0;
 }
